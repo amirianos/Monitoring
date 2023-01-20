f5b9c9dc33486db8374b6a5e805c8053d6029901
amir <amisoltani@pm.me>
2023-01-20 15:54:05 +0330

    update main file

--- main.go
+++ main.go
@@ -1,214 +1,97 @@
-/*package main
+package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+	//"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
-	"strconv"
-)
-
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func NewResponseWriter(w http.ResponseWriter) *responseWriter {
-	return &responseWriter{w, http.StatusOK}
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}
-
-var totalRequests = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "http_requests_total",
-		Help: "Number of get requests.",
-	},
-	[]string{"path"},
-)
-
-var responseStatus = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "response_status",
-		Help: "Status of HTTP response",
-	},
-	[]string{"status"},
-)
-
-var httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-	Name: "http_response_time_seconds",
-	Help: "Duration of HTTP requests.",
-}, []string{"path"})
-
-func prometheusMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		route := mux.CurrentRoute(r)
-		path, _ := route.GetPathTemplate()
-		fmt.Println(path)
-
-		rw := NewResponseWriter(w)
-		next.ServeHTTP(rw, r)
-
-		statusCode := rw.statusCode
-		statusCode = 200
-                fmt.Println(statusCode)
-		responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
-		//responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
-		totalRequests.WithLabelValues(path).Inc()
-
-	})
-}
-
-func init() {
-	prometheus.Register(totalRequests)
-	prometheus.Register(responseStatus)
-	prometheus.Register(httpDuration)
-}
-
-func main() {
-	router := mux.NewRouter()
-	router.Use(prometheusMiddleware)
-
-	// Prometheus endpoint
-	router.Path("/prometheus").Handler(promhttp.Handler())
-
-	// Serving static files
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-
-	fmt.Println("Serving requests on port 9000")
-	err := http.ListenAndServe(":9000", router)
-	log.Fatal(err)
-}
-
-
-package main
-
-import (
-	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
+	"strings"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	counter = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Namespace: "golang",
-			Name:      "my_counter",
-			Help:      "This is my counter",
-		})
-
-	gauge = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Namespace: "golang",
-			Name:      "my_gauge",
-			Help:      "This is my gauge",
-		})
-
-	histogram = prometheus.NewHistogram(
-		prometheus.HistogramOpts{
-			Namespace: "golang",
-			Name:      "my_histogram",
-			Help:      "This is my histogram",
-		})
-
-	summary = prometheus.NewSummary(
-		prometheus.SummaryOpts{
-			Namespace: "golang",
-			Name:      "my_summary",
-			Help:      "This is my summary",
-		})
-	
-	testmetric = prometheus.NewGauge(
-              prometheus.GaugeOpts{
-	             Name: "gauge test"
-	      })
+var runningcontainers = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "runningcontainers",
+	},
+	[]string{
+		"name",
+		"image",
+		"state",
+		"status",
+	},
+)
+var exitedcontainers = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "stopedcontainers",
+	},
+	[]string{
+		"name",
+		"image",
+		"state",
+		"status",
+	},
+)
+var runningcontainersnumber = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "runningcontainersnumber",
+	},
 )
 
+func update() {
+	number := 0
+	runningcontainers.Reset()
+	exitedcontainers.Reset()
+	//runningcontainersnumber.Reset()
+	cli, _ := client.NewEnvClient()
+	containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{
+		All: true,
+	})
+	for _, container := range containers {
+		container_status := string(container.Status[:])
+		container_name := strings.Join(container.Names, "")
+		container_image := string(container.Image[:])
+		container_state := string(container.State[:])
+		container_created := container.Created
+		//for running containers
+		if container_state == "running" {
+			runningcontainers.WithLabelValues(container_name, container_image, container_state, container_status).Set(float64(container_created))
+			number += 1
+		}
+		//for exited containers
+		if container_state == "exited" {
+			exitedcontainers.WithLabelValues(container_name, container_image, container_state, container_status).Set(float64(container_created))
+		}
+		runningcontainersnumber.Set(float64(number))
+	}
+}
 func main() {
-	rand.Seed(time.Now().Unix())
-	hello := prometheus.
-	histogramVec := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "prom_request_time",
-		Help: "Time it has taken to retrieve the metrics",
-	}, []string{"time"})
+	reg := prometheus.NewRegistry()
+	reg.Register(exitedcontainers)
+	reg.Register(runningcontainers)
+	reg.Register(runningcontainersnumber)
 
-	prometheus.Register(histogramVec)
-
-	http.Handle("/metrics", newHandlerWithHistogram(promhttp.Handler(), histogramVec))
-
-	prometheus.MustRegister(counter)
-	prometheus.MustRegister(gauge)
-	prometheus.MustRegister(histogram)
-	prometheus.MustRegister(summary)
-
+	router := mux.NewRouter()
+	router.Path("/prometheus").Handler(promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	ticker := time.NewTicker(10 * time.Second)
+	quit := make(chan struct{})
 	go func() {
 		for {
-			counter.Add(rand.Float64() * 5)
-			gauge.Add(rand.Float64()*15 - 5)
-			histogram.Observe(rand.Float64() * 10)
-			summary.Observe(rand.Float64() * 10)
-
-			time.Sleep(time.Second)
+			select {
+			case <-ticker.C:
+				update()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
 		}
 	}()
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func newHandlerWithHistogram(handler http.Handler, histogram *prometheus.HistogramVec) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		start := time.Now()
-		status := http.StatusOK
-		fmt.Println(status,"hello from other world")
-		defer func() {
-			histogram.WithLabelValues(fmt.Sprintf("%d", status)).Observe(time.Since(start).Seconds())
-		}()
-
-		if req.Method == http.MethodGet {
-			handler.ServeHTTP(w, req)
-			return
-		}
-		status = http.StatusBadRequest
-
-		w.WriteHeader(status)
-	})
-}*/
-
-package main
-import (
-    "fmt"
-)
-func main(){
-walk := []rune{'n','n','s','n','s','n','s','n','s','n','s'}
-  i := 0
-  j := 0
-  for _,k := range walk{
-    if k == 'n'{
-      j = j + 1
-    }
-    if k == 's'{
-      j = j - 1
-    }
-    if k == 'w'{
-      i = i + 1
-    }
-    if k == 'e'{
-      i = i - 1
-    }
-  }
-  if i==0 && j==0{
-    fmt.Println("true")
-  } else {
-  fmt.Println("false")
-  }
+	fmt.Println("Serving requests on port 5000")
+	err := http.ListenAndServe(":5000", router)
+	log.Fatal(err)
 }
